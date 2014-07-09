8ebe3348c770f4bc2ac2880fb65e053c2baf1deb
Andrew Murphy <andrewm1986@gmail.com>
2014-07-09 11:27:30 +0100

    Re-scafoled with MVC so we get accounts

--- Web/Properties/AssemblyInfo.cs
+++ Web/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("86d39ce6-5248-4a13-835d-10d5cd9b6fb1")]
+[assembly: Guid("530f24db-f267-4c78-a98e-7cbae83568bf")]
 
 // Version information for an assembly consists of the following four values:
 //
